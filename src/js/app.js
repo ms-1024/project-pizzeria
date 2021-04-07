bcc1c0a289999a101f76e0710471979eadb5dd83
Marcin Stalmach <mstalmach@op.pl>
2021-04-07 05:29:06 +0200

    Add correct to app.js

--- src/js/app.js
+++ src/js/app.js
@@ -13,7 +13,7 @@ const app = {
     const idFromHash = window.location.hash.replace('#/', '');
     // console.log('idFromHash:', idFromHash);
 
-    thisApp.activatePage(idFromHash);
+    let pageMatchingHash = thisApp.pages[0].id;
 
     for(let page of thisApp.pages){
       if(page.id == idFromHash){
