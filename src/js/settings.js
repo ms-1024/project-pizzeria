a6c94173dd6bc7c34b01a2f7496616cb64570af6
Marcin Stalmach <mstalmach@op.pl>
2021-04-10 07:32:08 +0200

    Add chapter 10.3

--- src/js/settings.js
+++ src/js/settings.js
@@ -64,6 +64,11 @@ export const select = {
     peopleAmount: '.people-amount',
     hoursAmount: '.hours-amount',
     tables: '.floor-plan .table',
+    planTables: '.floor-plan',
+    phone: '[name="phone"]',
+    address: '[name="address"]',
+    starters: '[name="starter"]',
+    form: '.booking-form',
   },
   nav: {
     links: '.main-nav a',
@@ -83,6 +88,7 @@ export const classNames = {
   booking: {
     loading: 'loading',
     tableBooked: 'booked',
+    tableSelected: 'selected',
   },
   nav: {
     active: 'active',
